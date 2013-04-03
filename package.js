b7636ef9034ed5dd5aff2243d4c8ec60c0e3ff81
Reio <reio@heathmont.net>
2013-04-04 02:22:03 +0300

    Update package.js

--- package.js
+++ package.js
@@ -3,17 +3,20 @@ Package.describe({
     summary : "TypeScript, a typed superset of Javascript that compiles to plain Javascript"
 });
 
-var ERROR = "\nTypeScript compilation failed!\n";
-ERROR = ERROR + (new Array(ERROR.length - 1).join("-")) + "\n";
-var fs = require('fs');
-
-// XXX Use other npm packages. Seen in the handlebars package ;)
-var execSync = require('exec-sync');
+Npm.depends({"exec-sync": "0.1.5"});
 
 Package.register_extension("ts", function (bundle, source_path, serve_path, where) {
-    var compileOut = source_path + '.compiled_typescript.js', // using `.js` as an extension would cause Meteor to load this file
-        compileCommand = 'tsc --out ' + compileOut + " " + source_path, // add client,server module type switch? 
-        bundlePath = serve_path + '.js',
+    var fs = Npm.require('fs');
+	var path = Npm.require('path');
+	var execSync = Npm.require('exec-sync');
+
+	var ERROR = "\nTypeScript compilation failed!\n";
+	ERROR = ERROR + (new Array(ERROR.length - 1).join("-")) + "\n";
+	// XXX Use other npm packages. Seen in the handlebars package ;)
+
+    var compileOut = source_path + '.compiled_typescript_js', // using `.js` as an extension would cause Meteor to load this file
+        compileCommand = 'tsc --nolib --sourcemap --out ' + compileOut + " " + source_path, // add client,server module type switch?
+        bundlePath = path.dirname(serve_path) + '/' + path.basename(serve_path, '.ts') + '.js',
         result = null;
 
     // Compile the TypeScript file with the TypeScript command line compiler. 
@@ -22,20 +25,50 @@ Package.register_extension("ts", function (bundle, source_path, serve_path, wher
     try {
         result = execSync(compileCommand);
     } catch (e) {
-        bundle.error(ERROR + e);
+	    result = true;
+		 console.log(e);
+//        bundle.error(ERROR + e);
     }
 
-    if (fs.existsSync(compileOut)) {
+    var jsPath = compileOut + '/' + path.basename(source_path, '.ts') + '.js';
+    var mapPath = compileOut + '/' + path.basename(source_path, '.ts') + '.js.map';
+    if (fs.existsSync(jsPath)) {
         if (result !== null) {
-            bundle.add_resource({
-                type : "js",
-                path : bundlePath,
-                data : new Buffer(fs.readFileSync(compileOut)),
-                where : where
-            });
+			  bundle.add_resource({
+				  type: "static", // WARNING : Seems to doesn't be updated on client reloading
+				  path : serve_path,
+				  data : new Buffer(fs.readFileSync(source_path)),
+				  where : where
+			  });
+			  bundle.add_resource({
+				  type: "static", // WARNING : Seems to doesn't be updated on client reloading
+				  path : bundlePath + '.map',
+				  data : new Buffer(fs.readFileSync(mapPath)),
+				  data : new Buffer(fs.readFileSync(mapPath).toString().replace('"sources":["../', '"sources":["' + path.dirname(serve_path) + '/')),
+				  where : where
+			  });
+			  bundle.add_resource({
+				  type : "js",
+				  path : bundlePath,
+				  data : new Buffer(fs.readFileSync(jsPath).toString().replace('//@ sourceMappingURL=', '//@ sourceMappingURL=' + path.dirname(serve_path) + '/')),
+				  where : where
+			  });
         }
         //Delete the created file afterwards and add the content to the bundle
-        fs.unlinkSync(compileOut);
+		 var rmDir = function(dirPath) {
+			 try { var files = fs.readdirSync(dirPath); }
+			 catch(e) { return; }
+			 if (files.length > 0)
+				 for (var i = 0; i < files.length; i++) {
+					 var filePath = dirPath + '/' + files[i];
+					 if (fs.statSync(filePath).isFile())
+						 fs.unlinkSync(filePath);
+					 else
+						 rmDir(filePath);
+				 }
+			 fs.rmdirSync(dirPath);
+		 };
+		 rmDir(compileOut);
     } else {
         bundle.error(ERROR);
     }
