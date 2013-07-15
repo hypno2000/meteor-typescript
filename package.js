d7c0aef306352bdc06eb367f6448766913ca89e5
Reio Piller <reio@heathmont.net>
2013-07-15 13:15:43 +0300

    dev

--- package.js
+++ package.js
@@ -8,19 +8,70 @@ Npm.depends({
 	"mkdirp": "0.3.5"
 });
 
+
+var path = Npm.require('path');
+var fs = Npm.require('fs');
+
+var refs = [];
+var dirs = [];
+function getReferences(dir) {
+	var res = "";
+	refs.forEach(function (entry) {
+		res += '///<reference path="' + path.relative(dir, entry) + '" />\n';
+	});
+	return res;
+}
+
 Package.register_extension("ts", function (bundle, source_path, serve_path, where) {
-	var fs = Npm.require('fs');
-	var path = Npm.require('path');
+	
+//	console.log("Src: " + source_path);
+	var isRef = source_path.substring(source_path.length - 5) === '.d.ts';
 	var mkdirp = Npm.require('mkdirp');
 
 	// cache check
+	//console.log(serve_path);
 	var cachePath = '.meteor/cache' + serve_path;
 	var cacheDir = path.dirname(cachePath);
 	var baseName = path.basename(source_path, '.ts');
 	var changeTime = fs.statSync(source_path).mtime;
-	if (!fs.existsSync(cacheDir)) {
+	var jsPath;
+	if (serve_path.substring(0, 10) === '/packages/') {
+		jsPath = cacheDir + '/' + path.dirname(serve_path.substring(10)) + '/' + baseName + '.js';
+	} else {
+		var rootPath = path.normalize(source_path.substring(0, source_path.length - serve_path.length) + '/..');
+		jsPath = cacheDir + '/' + path.dirname(path.relative(rootPath, source_path)) + '/' + baseName + '.js';
+	}
+	var mapPath = jsPath + '.map';
+
+	// references
+	var dir = path.dirname(source_path);
+	if (dirs.indexOf(dir) == -1) {
+		dirs.push(dir);
+	}
+	if (refs.indexOf(source_path) == -1) {
+		refs.push(source_path);
+		dirs.forEach(function (dir) {
+			var refData = getReferences(dir);
+//			console.log('path: ' + dir + "/.ref.d.ts");
+//			console.log('content:\n' + refData);
+			fs.writeFileSync(dir + "/.ref.d.ts", refData);
+//		    try {
+//		        fs.unlinkSync(dir + "/.ref.d.ts");
+//		    } catch(err) {
+//		        
+//		    }
+		});
+	}
+	
+	// dont compile d.ts files
+	if (isRef) {
+		return;
+	}
+
+	if (!fs.existsSync(cachePath)) {
 		mkdirp.sync(cacheDir);
 	}
+	
 	if (!fs.existsSync(cachePath) || changeTime.getTime() > fs.statSync(cachePath).mtime.getTime()) {
 
 		var execSync = Npm.require('exec-sync');
@@ -29,26 +80,30 @@ Package.register_extension("ts", function (bundle, source_path, serve_path, wher
 		ERROR = ERROR + (new Array(ERROR.length - 1).join("-")) + "\n";
 		// XXX Use other npm packages. Seen in the handlebars package ;)
 
-		var compileCommand = 'tsc --sourcemap --out ' + cacheDir + " " + source_path; // add client,server module type switch?
-//		var compileCommand = 'tsc --nolib --sourcemap --out ' + cacheDir + " " + source_path; // add client,server module type switch?
+		var compileCommand = 'tsc --sourcemap --out ' + cacheDir + ' ' + source_path; // add client,server module type switch?
+		//		var compileCommand = 'tsc --nolib --sourcemap --out ' + cacheDir + " " + source_path; // add client,server module type switch?
 		var result = null;
 
 		// Compile the TypeScript file with the TypeScript command line compiler.
 		// Until the TypeScript module provides a public API there is no reliable way around it without changing the
 		// TypeScript sources.
+
 		try {
 			result = execSync(compileCommand);
+			//console.log(compileCommand);
+			//console.log(result);
 		} catch (e) {
 
 			var lines = e.message.split('\n');
 			var errors = [];
 			for (var i = 0; i < lines.length; i++) {
 				if (
-					// !/The property '__super__' does not exist on value of type/.test(lines[i]) &&
-//					lines[i].substr(-36) !== 'Base type must be interface or class' &&
-//					lines[i].substr(-30) !== 'not exist in the current scope' &&
-//					lines[i].substr(-24) !== 'lacks an implementation.'
-					lines[i].indexOf('error TS2095') == -1
+				//					lines[i].trim() &&
+				// !/The property '__super__' does not exist on value of type/.test(lines[i]) &&
+				//					lines[i].substr(-36) !== 'Base type must be interface or class' &&
+				//					lines[i].substr(-30) !== 'not exist in the current scope' &&
+//									lines[i].substr(-24) !== 'lacks an implementation.'
+									lines[i].indexOf('error TS2095') == -1
 				) {
 					errors.push(lines[i]);
 				}
@@ -61,9 +116,10 @@ Package.register_extension("ts", function (bundle, source_path, serve_path, wher
 				result = true;
 		}
 
-		var jsPath = cacheDir + '/' + baseName + '.js';
-		var mapPath = cacheDir + '/' + baseName + '.js.map';
-
+		if (fs.existsSync(cacheDir + '/' + baseName + '.js')) {
+			jsPath = cacheDir + '/' + baseName + '.js';
+			mapPath = jsPath + '.map';
+		}
 		if (fs.existsSync(jsPath)) {
 			if (result !== null) {
 
@@ -90,7 +146,7 @@ Package.register_extension("ts", function (bundle, source_path, serve_path, wher
 			fs.unlinkSync(mapPath);
 		}
 		else {
-			bundle.error(ERROR);
+			bundle.error(ERROR + 'file was not created: \n' + jsPath + '\n' + serve_path + '\n' + cacheDir + '\n' + source_path);
 			return;
 		}
 
